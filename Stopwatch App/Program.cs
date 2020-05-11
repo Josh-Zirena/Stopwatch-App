90d9d9517dd0e0feac3191da931ddef17776ec24
Josh Zirena <JoshZirena@gmail.com>
2020-05-11 13:44:01 +0200

    feat: create stopwatch app

---
+++ Stopwatch App/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Stopwatch_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stopwatch = new Stopwatch();
+
+            Console.WriteLine("Stopwatch App");
+            Console.WriteLine("=================");
+            Console.WriteLine("Instructions..");
+            Console.WriteLine("Type 'start' to start the stopwatch.\nType 'stop' to stop the stopwatch.\nType 'quit' to quit the stopwatch.\n");
+            
+
+            while (true)
+            {
+                var input = Console.ReadLine();
+                Console.WriteLine("");
+                if (input.ToLower() == "start")
+                    stopwatch.Start();
+
+                else if (input.ToLower() == "stop")
+                    stopwatch.Stop();
+                else if (input.ToLower() == "quit")
+                    break;
+                else
+                {
+                    Console.WriteLine("Please enter 'start', 'stop', or 'quit' for functionality.\n");
+                }    
+            }
+        }
+    }
+}
---
+++ Stopwatch App/Stopwatch.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading;
+
+namespace Stopwatch_App
+{
+    public class Stopwatch
+    {
+        private bool _isRunning = false;
+        private DateTime _startTime;
+        private DateTime _endTime;
+
+        private TimeSpan duration
+        {
+            get
+            {
+                return _endTime - _startTime;
+            }
+        }
+
+        public void Start ()
+        {
+            if (_isRunning)
+                throw new InvalidOperationException("Cannot call the start method consecutively");
+
+            this._startTime = DateTime.Now;
+            Console.WriteLine("start time: " + this._startTime);
+
+            _isRunning = true;
+        }
+
+        public void Stop()
+        {
+            if (!_isRunning)
+                throw new InvalidOperationException("Cannot call the stop method consecutively");
+
+            this._endTime = DateTime.Now;
+            Console.WriteLine("end time: " + _endTime);
+            Console.WriteLine("duration: " + duration.TotalSeconds);
+            _isRunning = false;
+        }
+    }
+}
